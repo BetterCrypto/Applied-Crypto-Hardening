b393e0f6eb5bd73ab03fdd98b350eb4437b906a9
Aaron Kaplan <aaron@lo-res.org>
2013-12-10 00:09:08 +0100

    fix chomp in perl script

--- src/perlify.pl
+++ src/perlify.pl
@@ -6,16 +6,18 @@ use strict;
 my $debug=1;
 
 my $cipherStrB=`cat cipherStringB.txt`;
-
+chomp $cipherStrB;
 
 my @files=`find . -name "*.tex.template" -print`;
 my $f;
 
 foreach  $f ( @files)  {
-	print "file = $f\n" if $debug;
-	$f =~ /(.*\.tex\.)template/;
+	chomp $f;
+	$f =~ /(.*\.tex)\.template/;
 	my $ftex = $1;
 
+	print "file = $f\n" if $debug;
+	print "ftex = $ftex\n" if $debug;
 
 	open(FH,    "<", $f ) or die "could not open file $f: $!";
 	open(FHOUT, ">", $ftex ) or die "could not open file $ftex: $!";
