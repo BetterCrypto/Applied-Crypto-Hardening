3b00ae2daa6a49e6ff96e5c1cff72f0480c036af
Sebastian Pipping <sebastian@pipping.org>
2018-12-20 12:34:00 +0100

    ci: Copy build result in place

---
+++ tools/ci/copy-build.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+# Copyright (C) 2018 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under CC-BY-SA 3.0
+# https://creativecommons.org/licenses/by-sa/3.0/
+
+set -e
+set -u
+
+files_to_copy=(
+    src/applied-crypto-hardening.html
+    src/applied-crypto-hardening.pdf
+)
+target_directory="$1"
+
+PS4='# '
+set -x
+
+mkdir -p "${target_directory}"
+cp "${files_to_copy[@]}" "${target_directory}"/
